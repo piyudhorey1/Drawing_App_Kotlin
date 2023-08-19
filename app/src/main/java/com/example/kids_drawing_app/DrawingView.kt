07bfd6383ec5f3d0dffec6bc43b091c4f81e1fe1
P-CXR <pdhorey@cxr.agency>
2023-08-19 19:13:55 +0530

    Implemented the canvas for DrawingView.kt and setBrushSize function

---
+++ app/src/main/java/com/example/kids_drawing_app/DrawingView.kt
@@ -0,0 +1,108 @@
+package com.example.kids_drawing_app
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+import java.util.jar.Attributes
+
+class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.YELLOW
+    private var canvas: Canvas? = null
+    private val mPath = ArrayList<CustomPath>()
+
+    init{
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing(){
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.SQUARE
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        //mBrushSize = 20.toFloat()
+    }
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for(m in mPath){
+            mDrawPaint!!.strokeWidth = m.brushThickness
+            mDrawPaint!!.color = m.color
+            canvas.drawPath(m, mDrawPaint!!)
+        }
+
+        if(!mDrawPath!!.isEmpty){
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.moveTo(touchX, touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.lineTo(touchX, touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                mPath.add(mDrawPath!!)
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+
+    public fun setBrushSize(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path(){
+
+    }
+}
---
+++ app/src/main/java/com/example/kids_drawing_app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kids_drawing_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawing_view)
+        drawingView?.setBrushSize(20.toFloat())
+    }
+}
